3c2623ef8ed13b42cea8cce47c692972f4ebbda5
Eduarda Silva Neves <86806534+eduardasneves18@users.noreply.github.com>
2021-08-24 11:42:03 -0300

    alteações na base e templates

--- frontend/pages/urls.py
+++ frontend/pages/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
 
-from .views import AboutPageView, HomePageView
+from .views import AboutPageView, HomePageView, ContactsPageView
 
 #, ProdutoView, ProductsDetailView, category_list
 
 app_name = "pages"
 
 urlpatterns = [
-    path("about/", AboutPageView.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home"),
-    # path("", category_list, name="home"),
-    # path("products/", ProdutoView.as_view(), name="products"),
-    # path("products/detail", ProductsDetailView.as_view(), name="productDetail"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("contacts/", ContactsPageView.as_view(), name="contacts")
 
 ]
