e0fdccb5e4b57c232ca1854cb5fbc3b56ff5bdb0
MMedeiros03 <88498532+MMedeiros03@users.noreply.github.com>
2021-09-07 19:28:07 -0300

    criando as views do search

---
+++ backend/search/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from search.views import SearchProductView
+
+
+app_name = 'search'
+
+urlpatterns = [
+    path("",SearchProductView.as_view(),name='list'),
+]
--- backend/search/views.py
+++ backend/search/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import request
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
+from products.models import Product
+
 
 # Create your views here.
+class SearchProductView(ListView):
+    def get_queryset(self,*args,**kwargs):
+        request = self.request
+        result = request.GET
+        query = result.get('q',None)
+        if query is not None:
+            return Product.objects.filter(name__icontains = query)
+        return get_object_or_404
