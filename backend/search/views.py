44cae8520aa14fb99a061d209039e364a45293ac
MMedeiros03 <88498532+MMedeiros03@users.noreply.github.com>
2021-09-08 14:58:03 -0300

    criando mais uma view para o search

--- backend/search/views.py
+++ backend/search/views.py
@@ -6,6 +6,13 @@ from products.models import Product
 
 # Create your views here.
 class SearchProductView(ListView):
+    def get_context_data(self, *args,**kwargs):
+        context = super().get_context_data(*args,**kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context
+
+
     def get_queryset(self,*args,**kwargs):
         request = self.request
         result = request.GET
