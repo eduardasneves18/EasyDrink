c8ff65898103d627df429d97bbe0adbefa4ecc8d
Eduarda Silva Neves <86806534+eduardasneves18@users.noreply.github.com>
2021-09-14 17:13:09 -0300

    atualização frontend

--- frontend/users/views.py
+++ frontend/users/views.py
@@ -1,8 +1,15 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.shortcuts import  render, redirect
+from django.contrib.auth import login, authenticate #add this
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm #add this
 
 
-class UserPageView(TemplateView):
-    template_name = 'user/login.html'
+# class UserPageView(TemplateView):
+#     template_name = 'user/login.html'    
     
 
 class RegisterPageView(TemplateView):
@@ -10,3 +17,21 @@ class RegisterPageView(TemplateView):
 
 class ResetPageView(TemplateView):
     template_name = 'user/reset_password.html'
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			if password is not None:
+				return redirect("pages:home")
+			else:
+				messages.error(request, username)
+
+		else:
+			print('Senha correta')
+            
+	form = AuthenticationForm()
+	return render(request=request, template_name="user/login.html", context={"login_form":form})
